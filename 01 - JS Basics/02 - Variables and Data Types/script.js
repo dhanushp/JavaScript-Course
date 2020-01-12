45ad06b42ac301ae50c81d6227970bb8380b8f16
dhanushp <dhanushpb420@gmail.com>
2020-01-12 23:47:30 +0530

    Initial Commit

---
+++ 01 - JS Basics/02 - Variables and Data Types/script.js
@@ -0,0 +1,21 @@
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+var job = 'Teacher';
+console.log(job);
+
+// These are the variables
+/*
+multi
+line 
+comment
+*/
