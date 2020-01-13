244cc9c1f2f400b45608000d0bc0b147618c271d
dhanushp <dhanushpb420@gmail.com>
2020-01-13 15:37:04 +0530

    Added files

---
+++ 01 - JS Basics/05 - Operator Precedence/script.js
@@ -0,0 +1,31 @@
+// Operator Precedence
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+// Multiple Operator
+var isFullAge = now - yearJohn >= fullAge; //true
+console.log(isFullAge);
+
+
+// Grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
+
+// Multiple Assignments
+var x, y;
+x = y = (3 + 5) * 4 - 6; // 8 * 4 -6 // 32 - 6 //26 
+console.log(x, y); 
+
+// More OPerators 
+x *= 2
+console.log(x);
+x += 10;
+console.log(x);
+
+// Increament
+x++;
+console.log(x);
