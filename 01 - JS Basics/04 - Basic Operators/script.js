fb0585e02fbda3fd792c3ee390c2ad03292f1ff7
dhanushp <dhanushpb420@gmail.com>
2020-01-13 13:57:16 +0530

    Added files

---
+++ 01 - JS Basics/04 - Basic Operators/script.js
@@ -0,0 +1,32 @@
+// Basic Operators
+
+var now, yearJohn, yearMark;
+
+now = 2018
+ageJohn = 28;
+ageMark = 33;
+
+// Math Operators
+yearJohn = now - 28;
+yearMark = now - 33;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now - 2);
+console.log(now * 2);
+console.log(now / 2);
+
+
+// Logical Operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+
+
+// Typeof Operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+var x;
+console.log(typeof x);
