c3d267e1b5899f375890a23137636fe4ac9ca699
dhanushp <dhanushpb420@gmail.com>
2020-01-17 22:37:42 +0530

    Added files

---
+++ 01 - JS Basics/17 - Objects and Methods/script.js
@@ -0,0 +1,16 @@
+// Objects and Methods
+
+var john = {
+    firstName: 'John', 
+    lastName: 'Smith',
+    birthYear: 1990,
+    family: ['Jane', 'Mark', 'Bob', 'Emily'],
+    job: 'teacher',
+    isMarried: false,
+    calcAge: function() {
+        this.age = 2019 - this.birthYear;
+    }
+};
+
+john.calcAge();
+console.log(john);
