e3df8b547a912f325d1d1117d674594a601c7848
dhanushp <dhanushpb420@gmail.com>
2020-01-21 23:48:12 +0530

    Added files

---
+++ 01 - JS Basics/19 - Loops and Iteration/script.js
@@ -0,0 +1,11 @@
+// Loops and Iteration
+
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+var john = ['John', 'Smith', 1990, 'designer', false];
+
+for (var i = 0; i < john.length; i++) {
+    console.log(john[i]);
+}
