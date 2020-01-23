c49aaf114f85e89e39b906b544717b5cf9205c88
dhanushp <dhanushpb420@gmail.com>
2020-01-23 22:51:44 +0530

    Added files

--- 01 - JS Basics/19 - Loops and Iteration/script.js
+++ 01 - JS Basics/19 - Loops and Iteration/script.js
@@ -1,5 +1,5 @@
 // Loops and Iteration
-
+/*
 for (var i = 0; i < 10; i++) {
     console.log(i);
 }
@@ -9,6 +9,28 @@ var john = ['John', 'Smith', 1990, 'designer', false];
 for (var i = 0; i < john.length; i++) {
     console.log(john[i]);
 }
-Skfkfkf
-Dkfkfkf
-Ritmtk
+
+//While Loop
+var i = 0;
+while (i < john.length) {
+    console.log(john[i]);
+    i++;
+}
+*/
+
+// Continue and Break Statements
+var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') continue;
+    console.log(john[i]);
+}
+var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') break;
+    console.log(john[i]);
+}
+
+// Looping backwards
+for (var i = john.length; i >= 0; i--) {
+    console.log(john[i]);
+}
