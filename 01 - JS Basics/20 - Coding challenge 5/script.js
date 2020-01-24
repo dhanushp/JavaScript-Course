0d55031265d43b859656dd200e4434a09ce0fe2c
dhanushp <dhanushpb420@gmail.com>
2020-01-24 23:07:14 +0530

    Added files

--- 01 - JS Basics/20 - Coding challenge 5/script.js
+++ 01 - JS Basics/20 - Coding challenge 5/script.js
@@ -27,5 +27,52 @@ var john = {
     }
 }
 
+
+
+var mark = {
+    fullName: 'Mark Miller',
+    bills: [77, 475, 110, 45],
+    calcTips: function() {
+        this.tips = [];
+        this.finalValues = [];
+
+        for (var i = 0; i < this.bills.length; i++) 
+        {
+            // Determine percentage based on tipping rules
+            var percentage; 
+             var bill = this.bills[i];
+
+             if (bill < 100) {
+                 percentage = .2;
+             } else if (bill >= 100 && bill < 300) {
+                 percentage = .1;
+             } else {
+                 percentage = .25;
+             } 
+
+            // Add results to the corresponding arrays
+            this.tips[i] = bill * percentage;
+            this.finalValues[i] = bill + bill * percentage;
+
+        }
+    }
+}
+
+function calcAverrage(tips) {
+    var sum = 0;
+    for ( var i = 0; i < tips.length; i++) {
+        sum = sum +tips[i];
+    }
+    return sum / tips.length;
+}
+
+// Do the calculation
+mark.calcTips();
 john.calcTips();
-console.log(john);
+
+john.average = calcAverrage(john.tips);
+mark.average = calcAverrage(mark.tips);
+
+console.log(john, mark);
+
+john.average > mark.average ? console.log('John paid more tips') : console.log('Mark paid more tips');
