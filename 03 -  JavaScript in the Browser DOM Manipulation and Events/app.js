85b0d31e8d16e25761685b67e248aa5f7ca12412
dhanushp <dhanushpb420@gmail.com>
2020-01-29 20:35:49 +0530

    Added files

--- 03 -  JavaScript in the Browser DOM Manipulation and Events/app.js
+++ 03 -  JavaScript in the Browser DOM Manipulation and Events/app.js
@@ -17,12 +17,13 @@ activePlayer = 1;
 dice = Math.floor(Math.random() * 6) + 1;
 
 
-//document.querySelector('#current-' + activePlayer).textContent = dice;
-document.querySelector('#current-' + activePlayer).innerHTML = '<b>' + dice + '</b>';
+document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<b>' + dice + '</b>';
 
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
 
-
-
+document.querySelector('.dice').style.display = 'none';
 
 
 
