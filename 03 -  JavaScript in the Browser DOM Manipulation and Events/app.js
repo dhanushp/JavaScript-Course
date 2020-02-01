652156ffb8d24f834c97b1298cfd24d4d6bb61bf
dhanushp <dhanushpb420@gmail.com>
2020-02-01 23:03:07 +0530

    Added files

--- 03 -  JavaScript in the Browser DOM Manipulation and Events/app.js
+++ 03 -  JavaScript in the Browser DOM Manipulation and Events/app.js
@@ -34,7 +34,28 @@ document.querySelector('.btn-roll').addEventListener('click', function btn() {
 
 
     //3. Update the round score if the rolled number was NOT 1 
-    
+    if ( dice !== 1) {
+        // Add Score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        // Next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        // Set RoundScore back to zero
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0'
+        document.getElementById('current-1').textContent = '0'
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        //document.querySelector('.player-0-panel').classList.remove('active');
+        //document.querySelector('.player-1-panel').classList.add('active');
+
+        document.querySelector('.dice').style.display = 'none';
+
+    }
 
 });
 
