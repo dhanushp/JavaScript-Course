059d83b67fae1b3adf8299899df71bf1fe66a8d1
dhanushp <dhanushpb420@gmail.com>
2020-02-07 15:35:03 +0530

    Added files

---
+++ 05 - Next Generation JavaScript Intro to ES6  ES2015/script.js
@@ -0,0 +1,22 @@
+// Lecture: Let and Const
+
+// ES5
+var name5 = 'Jane Smith';
+var age5 = 23;
+name5 = 'Jane Miller';
+console.log(name5);
+
+// ES6
+let name6 = 'Jane Smith';
+let age6 = 23;
+name6 = 'Jane Miller';
+console.log(name6);
+
+
+
+let i = 23;
+for (let i = 0; i<5; i++) {
+    console.log(i);
+}
+
+console.log(i);
